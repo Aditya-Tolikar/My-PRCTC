b85fecd1f837ac019a1131eb7d42540b8f75693c
Aditya-Tolikar <adityatolikar@gmail.com>
2015-09-06 12:32:44 +0530

    Create function call.cpp

---
+++ function call.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+
+void main()
+{
+	long int func (void);
+	long int a;
+	
+	a = func();
+	cout<<a;
+}
+
+long int func (void)
+{
+	long int hsig;
+	hsig=87256;
+	return hsig;
+}
