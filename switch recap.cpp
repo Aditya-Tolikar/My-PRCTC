c664ba751c2b0d943570e6818d8e5df2f03ae128
Aditya-Tolikar <adityatolikar@gmail.com>
2015-09-06 12:33:32 +0530

    Create switch recap.cpp

---
+++ switch recap.cpp
@@ -0,0 +1,23 @@
+#include <iostream.h>
+
+void main()
+{
+int a;
+cin >> a;
+	switch(a)
+	{
+	case 1:
+	cout<<"1";
+	
+	case 2:
+	cout << "2";break;
+	
+	
+	case 3:
+	cout <<"3";
+	
+	default: cout<< "def";
+	
+
+	}
+}
