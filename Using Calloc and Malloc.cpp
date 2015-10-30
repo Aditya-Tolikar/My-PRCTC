8fa39867eb49a9690e14a42b4d4df41025ddd178
Aditya-Tolikar <adityatolikar@gmail.com>
2015-10-30 21:20:41 +0530

    Update and rename Using Calloc ad.cpp to Using Calloc and Malloc.cpp

--- Using Calloc and Malloc.cpp
+++ Using Calloc and Malloc.cpp
@@ -11,13 +11,14 @@ int main()
 	int size;
 	
 	cout<<"Size of data\t";cin>>size;
-	data = (int *) calloc (size,sizeof(int));
+//	data = (int *) calloc (size,sizeof(int));
+	data = (int *) malloc (size*sizeof(int));
 	
 	for(int i=0; i<size;i++)
 	{
 		//cin<<data[i];
 		scanf("%d",&data[i]);
-		//cin<< rendered unusable with calloc TDM C++
+		//cin rendered unusable with calloc &/| malloc
 	}
 	
 	for(int i=0; i<size;i++)
@@ -25,4 +26,10 @@ int main()
 		cout<<data[i]<<"\t";
 	}
 	
-}
+	free (data);
+	data = NULL;
+	
+	for(int i=0; i<size;i++)
+	{
+		cout<<mata[i]<<"\t";
+	}
